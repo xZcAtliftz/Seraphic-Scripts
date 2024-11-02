1002239ac6e887184c7dc3272e7f2570f8edbecd
xZcAtliftz Project <99465722+xZcAtliftz@users.noreply.github.com>
2024-11-02 19:06:55 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,12 @@
+    local gamelist = {
+        [1511883870]  = "https://raw.githubusercontent.com/xZcAtliftz/Seraphic-Scripts/main/Shindo-Life"
+    }
+    local checkgame = gamelist[game.gameId]
 
+    if gamelist[game.gameId] then
+        getgenv().AuthTime = tick()
+        print(checkgame)
+        loadstring(game:HttpGet(checkgame))()
+    else
+        game.Players.LocalPlayer:Kick("Game Not Support")
+    end
